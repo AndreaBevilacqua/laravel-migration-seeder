c1dda5724b087e3702533ef8e1a4f1e3330d2a87
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-03-12 15:49:03 +0100

    added base template

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -1,3 +1,34 @@
-<div>
-    <!-- No surplus words or unnecessary actions. - Marcus Aurelius -->
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{env('APP_NAME')}} | @yield('title')</title>
+    
+{{-- Vite --}}
+@vite('resources/js/app.js')
+
+{{-- Cdns --}}
+@yield('cdns')
+
+</head>
+<body>
+{{-- Header --}}
+@include('includes.header')
+
+{{-- Main --}}
+<main>
+    @yield('core-content')
+</main>
+
+{{-- Bottom-Footer --}}
+@include('includes.bottomfooter')
+
+{{-- Top-Foooter --}}
+@include('includes.topfooter')
+
+{{-- Script --}}
+@yield('script')
+</body>
+</html>
